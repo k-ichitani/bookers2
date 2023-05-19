300a8900afcf15ef6d17d22ee4fba49352666ad1
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-05-19 09:39:59 +0000

    202305191839

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @users = User.all
     @books = @user.books
+    @newbook = Book.new
   end
 
   def edit
@@ -10,6 +12,22 @@ class UsersController < ApplicationController
 
   def index
     @user = current_user
+    @user_id = User.find(params[:id])
     @users = User.all
+    @books = @user.books
+    @newbook = Book.new
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
