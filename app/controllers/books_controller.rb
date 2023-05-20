2d8eb27013d72327675486a1083f3c0e08bce992
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-05-20 11:14:10 +0000

    202305202014

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,14 +4,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
-    book.user_id = current_user.id
-    if book.save
-      redirect_to book_path(book.id)
+    @newbook = Book.new(book_params)
+    @newbook.user_id = current_user.id
+    if @newbook.save
+      redirect_to book_path(@newbook.id)
     else
       @books = Book.all
       @user = current_user
-      @newbook = Book.new
       render :index
     end
   end
@@ -36,8 +35,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
