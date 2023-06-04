ff7450e6b4063b7e429cc25bf2fee79bc5324b60
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-06-04 07:01:59 +0000

    20230604非同期通信機能 未

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,14 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer ここは非同期通信の場合削除要！
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer　ここは非同期通信の場合削除要！
   end
 
 
