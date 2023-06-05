f27dab19250cbc4b30b2caf42802489169296e0f
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-06-05 04:11:13 +0000

    20230605コメント非同期 未

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    # redirect_to request.referer　非同期通信の場合削除することでそのアクションのjsファイルを探しに行く
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to request.referer
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
+    @book = Book.find(params[:book_id])
+    # redirect_to request.referer　非同期通信の場合削除
   end
 
   private
