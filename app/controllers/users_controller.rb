85699d6a11165b01a7fd97289b519e3874749741
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-06-06 12:19:15 +0000

    20230606 7日間分の投稿数チャート,ビューから

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @users = User.all
-    @books = @user.books
     @newbook = Book.new
+    @books = @user.books
+    @today_book = @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def edit
