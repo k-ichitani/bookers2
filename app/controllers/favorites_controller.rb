1b1ec26536a9a6b2638e4a98429de62cd5f36554
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-06-09 07:57:31 +0000

    20230609 いいね順

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,16 @@
 class FavoritesController < ApplicationController
 
   def create
-    @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: @book.id)
-    favorite.save
+    book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.new(book_id: book.id)
+    @favorite.save
     # redirect_to request.referer ここは非同期通信の場合削除要！
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: @book.id)
-    favorite.destroy
+    book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: book.id)
+    @favorite.destroy
     # redirect_to request.referer　ここは非同期通信の場合削除要！
   end
 
