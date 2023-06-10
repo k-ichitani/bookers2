ee178f9bb51375db71328c8c81809b35a3f292fd
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-06-10 06:26:24 +0000

    20230610 グループ作成機能start

---
+++ app/models/group.rb
@@ -0,0 +1,14 @@
+class Group < ApplicationRecord
+  has_many :group_users
+  has_many :users, through: group_users
+  belongs_to :owner, class_name: 'User'
+  
+  validates :name, presence: true
+  validates :introduction, presence: true
+  has_one_attached :group_image
+  
+  def is_owned_by?(user)
+    owner.id == user.id
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     resource:favorite, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  resources :groups, only: [:new, :index, :show, :create, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
