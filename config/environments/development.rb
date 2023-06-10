8a442f0a8c11cf05cd46cb529ae9e8a49df5045a
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-06-10 14:49:04 +0000

    202306102348

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  # falseからtrueに変更！
 
   config.action_mailer.perform_caching = false
 
