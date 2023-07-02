60b5d4131306ed03fbe1b3251bf565af4a43c674
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-07-02 03:26:50 +0000

    add ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,10 @@ Rails.application.routes.draw do
     get 'new/mail' => 'groups#new_mail'
     get 'send/mail' => 'groups#send_mail'
   end
+  
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
